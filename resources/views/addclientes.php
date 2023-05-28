3ecd36b6b2efd7a4b1dbe7f0d76b8bab6c6ed43e
Jenny Sofia Moreno <jennysmr2014@gmail.com>
2023-05-28 11:11:59 -0500

    Vista de AddClientes

---
+++ resources/views/addclientes.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Añadir Clientes</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    <!-- Navbar y Header-->
+    <nav class="nav-extended teal lighten-2">
+        <div class="nav-wrapper">
+        <a href="#!" class="brand-logo center"><i class="material-icons">shopping_bag</i>Tienda de Ropa</a>
+        </div>    
+    </nav>
+    <div class="container">
+        <h4>Sé nuestro cliente</h2>
+        <h6>Para nosotros sería genial recomendarte de nuestras ofertas y descuentos. Registrate y no te pierdas de las mejores promociones</h6>
+        <br>
+        
+        <div class="row">
+            <form class="col s12">
+                <div class="row">
+                    <div class="input-field col s6">
+                    <input id="last_name" type="text" class="validate">
+                    <label for="last_name">Nombre</label>
+                    </div>
+                    <div class="input-field col s6">
+                    <input id="last_name" type="text" class="validate">
+                    <label for="last_name">Apellido</label>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</html>
