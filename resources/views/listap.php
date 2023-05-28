5a38c4b65296417672e38b4044195b186893ceda
Jenny Sofia Moreno <jennysmr2014@gmail.com>
2023-05-28 12:47:46 -0500

    Vista: Lista

--- resources/views/listap.php
+++ resources/views/listap.php
@@ -4,9 +4,62 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Lista de Productos</title>
 </head>
 <body>
-    <h3>Lista de Productos Disponibles</h3>
+    <!-- Navbar y Header-->
+    <nav class="nav-extended teal lighten-2">
+        <div class="nav-wrapper">
+        <a href="#!" class="brand-logo center"><i class="material-icons">shopping_bag</i>Tienda de Ropa</a>
+        </div>    
+    </nav>
+
+    <div class="container">
+        <h4 class="center">Listado de Productos</h4>
+        <h6 class="center">Escoge la categoría de productos que quieres visualizar</h6>
+        <br>
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://static.vecteezy.com/system/resources/previews/015/238/446/non_2x/sneakers-with-isolated-colored-background-shoe-object-illustration-vector.jpg">
+                        <span class="card-title black-text">Zapatos</span>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Ver</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://media.istockphoto.com/id/1072482542/es/vector/hipster-hombre-vector-avatar.jpg?s=170667a&w=0&k=20&c=--XfsZemsoMdhJlI1LAbc33DvqAUPmrJDimyWI9vVAs=">
+                        <span class="card-title black-text">Ropa de Hombre</span>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Ver</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://png.pngtree.com/png-clipart/20221005/ourlarge/pngtree-long-haired-woman-vector-png-image_6282089.png">
+                        <span class="card-title black-text">Ropa de Mujer</span>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Ver</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </html>
