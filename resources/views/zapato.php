5038c1540a8caa4adc845db929c64e3bfca2af9c
Hedaco <113945165+Hedaco@users.noreply.github.com>
2023-05-28 12:11:29 -0500

    vista zapato

--- resources/views/zapato.php
+++ resources/views/zapato.php
@@ -5,8 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado de Zapatos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
-    <h3>Listado de Zapatos</h3>
+    <nav class="nav-extended teal lighten-2">
+        <div class="nav-wrapper">
+        <a href="#!" class="brand-logo center"><i class="material-icons">shopping_bag</i>Tienda de Ropa</a>
+        </div>    
+    </nav>
+    <div class="bg-gray-800 text-gray-300 max-w-xs rounded-2xl overflow-hidden">
+        <img src="https://media.gq.com.mx/photos/63a33e673a892f6e0ec57e6e/3:2/w_1938,h_1292,c_limit/Nike-Dunk-mid-panda.png" alt="shoe">
+        <div class="px-6 py-6 ">
+            <h2 class="text-xl">Tenis</h2>
+            <p class="text-sm">Tenis referencia 2121</p>
+            <button class="bg-indigo-500 py-2 px-3 rounded-lg mt-4 ml-auto block">
+                comprar
+            </button>
+        </div>
+    </div>
 </body>
 </html>
