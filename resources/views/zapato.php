450a7d974900c1a20a0e7129ca45f0457e29dd3c
Hedaco <113945165+Hedaco@users.noreply.github.com>
2023-05-28 12:43:53 -0500

    view zapato2

--- resources/views/zapato.php
+++ resources/views/zapato.php
@@ -14,15 +14,104 @@
         <a href="#!" class="brand-logo center"><i class="material-icons">shopping_bag</i>Tienda de Ropa</a>
         </div>    
     </nav>
-    <div class="bg-gray-800 text-gray-300 max-w-xs rounded-2xl overflow-hidden">
-        <img src="https://media.gq.com.mx/photos/63a33e673a892f6e0ec57e6e/3:2/w_1938,h_1292,c_limit/Nike-Dunk-mid-panda.png" alt="shoe">
-        <div class="px-6 py-6 ">
-            <h2 class="text-xl">Tenis</h2>
-            <p class="text-sm">Tenis referencia 2121</p>
-            <button class="bg-indigo-500 py-2 px-3 rounded-lg mt-4 ml-auto block">
-                comprar
-            </button>
+
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card blue lighten-4">
+                    <div class="card-image ">
+                        <img class="responsive-img" src="https://media.gq.com.mx/photos/63a33e673a892f6e0ec57e6e/3:2/w_1938,h_1292,c_limit/Nike-Dunk-mid-panda.png" alt="shoe">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Tenis</span>
+                        <p>Tenis referencia 2121</p>
+                    </div>
+                    <div class="card-action">
+                        <a class="waves-effect waves-light btn">Comprar</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img class="responsive-img" src="https://media.gq.com.mx/photos/63a33e673a892f6e0ec57e6e/3:2/w_1938,h_1292,c_limit/Nike-Dunk-mid-panda.png" alt="shoe">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Tenis</span>
+                        <p>Tenis referencia 2121</p>
+                    </div>
+                    <div class="card-action">
+                        <a class="waves-effect waves-light btn">Comprar</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img class="responsive-img" src="https://media.gq.com.mx/photos/63a33e673a892f6e0ec57e6e/3:2/w_1938,h_1292,c_limit/Nike-Dunk-mid-panda.png" alt="shoe">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Tenis</span>
+                        <p>Tenis referencia 2121</p>
+                    </div>
+                    <div class="card-action">
+                        <a class="waves-effect waves-light btn">Comprar</a>
+                    </div>
+                </div>
+            </div>
         </div>
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img class="responsive-img" src="https://media.gq.com.mx/photos/63a33e673a892f6e0ec57e6e/3:2/w_1938,h_1292,c_limit/Nike-Dunk-mid-panda.png" alt="shoe">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Tenis</span>
+                        <p>Tenis referencia 2121</p>
+                    </div>
+                    <div class="card-action">
+                        <a class="waves-effect waves-light btn">Comprar</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img class="responsive-img" src="https://media.gq.com.mx/photos/63a33e673a892f6e0ec57e6e/3:2/w_1938,h_1292,c_limit/Nike-Dunk-mid-panda.png" alt="shoe">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Tenis</span>
+                        <p>Tenis referencia 2121</p>
+                    </div>
+                    <div class="card-action">
+                        <a class="waves-effect waves-light btn">Comprar</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img class="responsive-img" src="https://media.gq.com.mx/photos/63a33e673a892f6e0ec57e6e/3:2/w_1938,h_1292,c_limit/Nike-Dunk-mid-panda.png" alt="shoe">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Tenis</span>
+                        <p>Tenis referencia 2121</p>
+                    </div>
+                    <div class="card-action">
+                        <a class="waves-effect waves-light btn">Comprar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
     </div>
+    
+    
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </html>
